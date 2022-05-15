7d6c4d28f0646cd00bc6674a30904982c1b17162
Kashif <km092009@gmail.com>
2022-05-14 22:23:59 -0400

    Navmenu implemented

---
+++ src/pages/Inputs.jsx
@@ -0,0 +1,14 @@
+const SelectInput = () => {
+  return ( <select>
+    <option value="">PHP</option>
+    <option value="">Dejango</option>
+    <option value="">Node</option>
+  </select> );
+}
+
+
+const TextInput = () => {
+  return <input type="text" />
+}
+
+export {SelectInput, TextInput};
